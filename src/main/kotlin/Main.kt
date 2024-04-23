50c954609de255cb6c7cbbef06fa2d6251c7243b
Kyle Dominic Galima <kyle.galima@siteminder.com>
2024-04-23 14:04:38 +0800

    kotlin-exer-01

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,112 @@
-package org.example
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println(calculateFactorial(4))
+    println(checkPrime(2))
+    println(reverseString("Hello"))
+    println(maxInArray(arrayOf(1, 5, 3, 2, 4)))
+    println(fibonacciSequence(10))
+    println(checkPalindrome("olo"))
+    println(sumOfDigitsInANumber(27))
+    println(getGreatestCommonDivisor(20,5))
+    println(countVowels("kyle dominic"))
+    println(celsiusToFahrenheit(0.0))
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun calculateFactorial(num: Int): Int {
+    require(num >= 0) { "Number can't be negative" }
+
+    return if (num <= 1) {
+        1
+    } else {
+        num * calculateFactorial(num - 1)
     }
 }
+
+fun checkPrime(num: Int): String {
+    if (num <= 1) {
+        return "$num is not Prime"
+    }
+
+    for (i in 2 until num) {
+        if (num % i == 0) {
+            return "$num is not Prime"
+        }
+    }
+    return "$num is Prime"
+}
+
+fun reverseString(str: String): String {
+    return "$str reversed is ${str.reversed()}"
+}
+
+fun <T: Comparable<T>>maxInArray(arr: Array<T> ): T? {
+    return arr.maxOrNull()
+}
+
+fun fibonacciSequence(num: Int): List<Int> {
+    val sequenceList = mutableListOf<Int>()
+
+    var a = 0
+    var b = 1
+
+    for (i in 1..num ){
+        sequenceList.add(a)
+        val sum = a + b
+        a = b
+        b = sum
+    }
+
+    return sequenceList
+}
+
+fun checkPalindrome(str: String): String {
+    return if (str == str.reversed()) {
+        "$str is a Palindrome"
+    } else {
+        "$str is NOT a Palindrome"
+    }
+}
+
+fun sumOfDigitsInANumber(num: Int): String {
+    var number = num
+    var sum = 0
+
+    while (number > 0) {
+        sum += number % 10
+        number /= 10
+    }
+
+    return "The sum of all digits in $num is $sum"
+}
+
+fun getGreatestCommonDivisor(a: Int, b: Int): String {
+    var num1 = a
+    var num2 = b
+
+    while (num2 != 0) {
+        val temp = num2
+        num2 = num1 % num2
+        num1 = temp
+    }
+    return "The Greatest Common Divisor of $a and $b is $num1"
+}
+
+fun countVowels(str: String): String {
+    val vowels = setOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+    var count = 0
+
+    for (char in str) {
+        if (char in vowels) {
+            count++
+        }
+    }
+
+    return "There are $count vowels in $str"
+}
+
+fun celsiusToFahrenheit(celsius: Double): String {
+    val fahrenheit = celsius * 9 / 5 + 32
+
+    return "$celsius celsius is equal to $fahrenheit fahrenheit."
+}
+
+
